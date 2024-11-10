6e3841d0f31cd34cdfdc4e4c9aa18961a62ad3f5
Rafael Santos <rafa.05125@gmail.com>
2024-11-10 11:45:42 -0300

    Requests pattern

---
+++ Dima.Core/Requests/PagedRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Dima.Core.Requests
+{
+    public class PagedRequest : Request
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 25;
+    }
+}

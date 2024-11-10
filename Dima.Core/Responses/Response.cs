499fda527b655d6eef866083fba7a4a79760f69d
Rafael Santos <rafa.05125@gmail.com>
2024-11-10 12:00:30 -0300

    Response constructors and serialization

--- Dima.Core/Responses/Response.cs
+++ Dima.Core/Responses/Response.cs
@@ -1,17 +1,29 @@
-﻿namespace Dima.Core.Responses
+﻿using System.Text.Json.Serialization;
+
+namespace Dima.Core.Responses
 {
-    public class Response
+    public class Response<TData>
     {
+        private const int DefaultStatusCode = 200;
         private readonly int _code;
-        public Response(string? data, string message, int code)
+
+        [JsonConstructor]
+        public Response() => _code = DefaultStatusCode;
+
+        public Response(
+            TData? data,
+            int code = DefaultStatusCode,
+            string? message = null)
         {
             Data = data;
             Message = message;
             _code = code;
         }
-        public string? Data { get; set; }
-        public string Message { get; set; } = string.Empty;
-        public bool IsSuccess 
+        public TData? Data { get; set; }
+        public string? Message { get; set; }
+
+        [JsonIgnore]
+        public bool IsSuccess
             => _code is >= 200 and <= 299;
     }
 }

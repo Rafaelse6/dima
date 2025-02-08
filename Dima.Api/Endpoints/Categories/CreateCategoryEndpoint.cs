c2337448741090e7f792049a0e86251ebc3724c7
Rafael Santos <rafa.05125@gmail.com>
2025-02-08 11:34:55 -0300

    Endpoints pattern

---
+++ Dima.Api/Endpoints/Categories/CreateCategoryEndpoint.cs
@@ -0,0 +1,12 @@
+﻿using Dima.Api.Common.Api;
+
+namespace Dima.Api.Endpoints.Categories
+{
+    public class CreateCategoryEndpoint : IEndpoint
+    {
+        public static void Map(IEndpointRouteBuilder app)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

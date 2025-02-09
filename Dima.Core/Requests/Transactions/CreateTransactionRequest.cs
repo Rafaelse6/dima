36251260798075dea188f6a328872d43628dd920
Rafael Santos <rafa.05125@gmail.com>
2025-02-09 11:47:32 -0300

    Transactions - Requests

---
+++ Dima.Core/Requests/Transactions/CreateTransactionRequest.cs
@@ -0,0 +1,24 @@
+﻿using Dima.Core.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dima.Core.Requests.Transactions
+{
+    public class CreateTransactionRequest : Request
+    {
+        [Required(ErrorMessage = "Título Inválido")]
+        public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Tipo Inválido")]
+        public ETransactionType Type { get; set; }
+        
+        [Required(ErrorMessage = "Quantidade Inválida")]
+        public decimal Amount { get; set; }
+        
+        [Required(ErrorMessage = "Categoria Inválida")]
+        public long CategoryId { get; set; }
+        
+        [Required(ErrorMessage = "Data Inválida")]
+        public DateTime? PaidOrReceivedAt { get; set; }
+
+    }
+}

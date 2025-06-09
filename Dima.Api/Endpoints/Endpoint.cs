6a7df94c132db77f85b918be016ebb101469ae7b
Rafael Santos <rafa.05125@gmail.com>
2025-06-09 00:16:41 -0300

    Refactoring Identity Endpoints

--- Dima.Api/Endpoints/Endpoint.cs
+++ Dima.Api/Endpoints/Endpoint.cs
@@ -1,6 +1,8 @@
 ﻿using Dima.Api.Common.Api;
 using Dima.Api.Endpoints.Categories;
+using Dima.Api.Endpoints.Identity;
 using Dima.Api.Endpoints.Transactions;
+using Dima.Api.Models;
 
 namespace Dima.Api.Endpoints
 {
@@ -33,6 +35,15 @@ namespace Dima.Api.Endpoints
                .MapEndpoint<DeleteTransactionEndpoint>()
                .MapEndpoint<GetTransactionByIdEndpoint>()
                .MapEndpoint<GetTransactionByPeriodEndpoint>();
+
+            endpoints.MapGroup("v1/identity")
+                .WithTags("Identity")
+                .MapIdentityApi<User>();
+
+            endpoints.MapGroup("v1/identity")
+                .WithTags("Identity")
+                .MapEndpoint<LogoutEndpoint>()
+                .MapEndpoint<GetRolesEndpoint>();
         }
 
         private static IEndpointRouteBuilder MapEndpoint<TEndpoint>(this IEndpointRouteBuilder app)

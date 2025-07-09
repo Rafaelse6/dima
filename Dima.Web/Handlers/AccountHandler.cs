ed8284d3f1a7ebc9865c656f07a488d2d38723f3
Rafael Santos <rafa.05125@gmail.com>
2025-07-09 12:09:24 -0300

    AccountService - Register and Logout

--- Dima.Web/Handlers/AccountHandler.cs
+++ Dima.Web/Handlers/AccountHandler.cs
@@ -2,6 +2,7 @@
 using Dima.Core.Requests.Account;
 using Dima.Core.Responses;
 using System.Net.Http.Json;
+using System.Text;
 
 namespace Dima.Web.Handlers
 {
@@ -17,14 +18,18 @@ namespace Dima.Web.Handlers
                 : new Response<string>(null, 400, "Não foi possível realizar o login");
         }
 
-        public Task<Response<string>> RegisterAsync(RegisterRequest request)
+        public async Task<Response<string>> RegisterAsync(RegisterRequest request)
         {
-            throw new NotImplementedException();
+            var result = await _client.PostAsJsonAsync("v1/identity/register", request);
+            return result.IsSuccessStatusCode
+                ? new Response<string>("Cadastro realizado com sucesso", 201, "Cadastro realizado com sucesso")
+                : new Response<string>(null, 400, "Não foi possível realizar o seu cadastro");
         }
 
-        public Task LogoutAsync()
+        public async Task LogoutAsync()
         {
-            throw new NotImplementedException();
+            var emptyContent = new StringContent("{}", Encoding.UTF8, "application/json");
+            await _client.PostAsJsonAsync("v1/identity/logout", emptyContent);
         }
     }
 }

b143d06e10e251c6a4d480ffbf250b4c58342fe0
Rafael Santos <rafa.05125@gmail.com>
2025-07-09 11:55:49 -0300

    HttpClientFactory

---
+++ Dima.Web/Handlers/AccountHandler.cs
@@ -0,0 +1,27 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Requests.Account;
+using Dima.Core.Responses;
+using System.Net.Http.Json;
+
+namespace Dima.Web.Handlers
+{
+    public class AccountHandler(IHttpClientFactory httpClientFactory) : IAccountHandler
+    {
+        private readonly HttpClient _client = httpClientFactory.CreateClient(Configuration.HttpClientName);
+
+        public async Task<Response<string>> LoginAsync(LoginRequest request)
+        {
+            await _client.PostAsJsonAsync("v1/identity/login");
+        }
+
+        public Task<Response<string>> RegisterAsync(RegisterRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task LogoutAsync()
+        {
+            throw new NotImplementedException();
+        }       
+    }
+}

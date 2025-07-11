9c433ad44c94029dfff8cef19c1b8f5abfbe542c
Rafael Santos <rafa.05125@gmail.com>
2025-07-11 18:06:19 -0300

    Claims

--- Dima.Web/Security/CookieAuthenticationStateProvider .cs
+++ Dima.Web/Security/CookieAuthenticationStateProvider .cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
+﻿using Dima.Core.Models.Account;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Json;
+using System.Security.Claims;
 
 namespace Dima.Web.Security
 {
-    public class CookieAuthenticationStateProvider(IHttpClientFactory clientFactory) : 
-        AuthenticationStateProvider, 
+    public class CookieAuthenticationStateProvider(IHttpClientFactory clientFactory) :
+        AuthenticationStateProvider,
         ICookieAuthenticationStateProvider
     {
         private bool _isAuthenticated = false;
@@ -15,11 +18,38 @@ namespace Dima.Web.Security
             return _isAuthenticated;
         }
 
+        public void NotifyAuthenticationStateChanged() => NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+
+
         public override Task<AuthenticationState> GetAuthenticationStateAsync()
         {
+
             throw new NotImplementedException();
         }
 
-        public void NotifyAuthenticationStateChanged() => NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+        private async Task<User?> GetUser()
+        {
+            try
+            {
+                return await _client.GetFromJsonAsync<User>("v1/identity/manage/info");
+
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private async Task<List<Claim>> GetClaims(User user)
+        {
+
+            var claims = new List<Claim>()
+            {
+                new(ClaimTypes.Name, user.Email),
+                new(ClaimTypes.Email, user.Email),
+            };
+
+            return claims;
+        }
     }
 }

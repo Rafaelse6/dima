af14ac2aa40247d9fcc77df0eac0cbfbc91667b3
Rafael Santos <rafa.05125@gmail.com>
2025-07-16 23:56:30 -0300

    Inject

--- Dima.Web/Pages/Identity/Register.razor.cs
+++ Dima.Web/Pages/Identity/Register.razor.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using MudBlazor;
 
 namespace Dima.Web.Pages.Identity
 {
     public partial class RegisterPage : ComponentBase
     {
 
+        public MudForm MudForm
+        {
+            get; set;
+        } = new MudForm();
     }
 }

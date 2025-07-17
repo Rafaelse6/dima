9eaa9c3037ba63b3f1560d4f7765070741a349d5
Rafael Santos <rafa.05125@gmail.com>
2025-07-17 00:55:38 -0300

    Dependencies

--- Dima.Web/Pages/Identity/Register.razor.cs
+++ Dima.Web/Pages/Identity/Register.razor.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Dima.Core.Handlers;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using MudBlazor;
 
@@ -7,9 +8,20 @@ namespace Dima.Web.Pages.Identity
     public partial class RegisterPage : ComponentBase
     {
 
-        public MudForm MudForm
-        {
-            get; set;
-        } = new MudForm();
+        #region Dependencies
+        [Inject]
+        public ISnackbar Snackbar { get; set; } = null!;
+
+        [Inject]
+        public IAccountHandler Handler { get; set; } = null!;
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = null!;
+
+        [Inject]
+        public AuthenticationStateProvider AuthenticationStateProvider { get; set; } = null!;
+
+        #endregion
+
     }
 }

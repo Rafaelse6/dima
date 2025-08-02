cbeb2fb4097d3f6a592966dcd4b49d65f2abd4af
Rafael Santos <rafa.05125@gmail.com>
2025-08-01 22:25:56 -0300

    Getting a Category

---
+++ Dima.Web/Pages/Categories/Edit.razor.cs
@@ -0,0 +1,50 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Requests.Categories;
+using Microsoft.AspNetCore.Components;
+
+namespace Dima.Web.Pages.Categories
+{
+    public partial class EditCategoryPage : ComponentBase
+    {
+
+        #region Properties
+        public bool IsBusy { get; set; } = false;
+        public UpdateCategoryRequest InputModel { get; set; } = new();
+        #endregion
+
+        #region Parameters
+        [Parameter]
+        public string Id { get; set; } = string.Empty;
+        #endregion
+
+        #region Services
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = null!;
+
+        [Inject]
+        public ICategoryHandler Handler { get; set; } = null!;
+
+        #endregion
+
+        #region Override
+
+        protected override async Task OnInitializedAsync()
+        {
+            var request = new GetCategoryByIdRequest
+            {
+                Id = long.Parse(Id)
+            };
+
+            var response = await Handler.GetByIdAsync(request);
+            if (response.IsSuccess && response.Data is not null)
+                InputModel = new UpdateCategoryRequest
+                {
+                    Id = response.Data.Id,
+                    Title = response.Data.Title,
+                    Description = response.Data.Description
+                };
+        }
+        #endregion
+    }
+}

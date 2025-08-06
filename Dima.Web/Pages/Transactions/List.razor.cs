d30cf74f8b1b4460962ac21a98086712fc555047
Rafael Santos <rafa.05125@gmail.com>
2025-08-06 09:41:50 -0300

    MudDataGrid transactions

--- Dima.Web/Pages/Transactions/List.razor.cs
+++ Dima.Web/Pages/Transactions/List.razor.cs
@@ -1,5 +1,7 @@
-﻿using Dima.Core.Handlers;
+﻿using Dima.Core.Common.Extensions;
+using Dima.Core.Handlers;
 using Dima.Core.Models;
+using Dima.Core.Requests.Transactions;
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
 
@@ -35,6 +37,57 @@ namespace Dima.Web.Pages.Transactions
 
         [Inject]
         public ITransactionHandler Handler { get; set; } = null!;
-        #endregion 
+        #endregion
+
+        #region Override 
+
+        protected override async Task OnInitializedAsync()
+        => await GetTransactions();
+
+        #endregion
+
+        #region Public Methods
+
+        public Func<Transaction, bool> Filter => transaction =>
+        {
+            if (string.IsNullOrEmpty(SearchTerm))
+                return true;
+
+            return transaction.Id.ToString().Contains(SearchTerm, StringComparison.OrdinalIgnoreCase)
+                    || transaction.Title.Contains(SearchTerm, StringComparison.OrdinalIgnoreCase);
+        };
+        #endregion
+        #region Private Methods
+
+        private async Task GetTransactions()
+        {
+            IsBusy = true;
+
+            try
+            {
+                var request = new GetTransactionsByPeriodRequest
+                {
+                    StartDate = DateTime.Now.GetFirstDay(CurrentYear, CurrentMonth),
+                    EndDate = DateTime.Now.GetLastDay(CurrentYear, CurrentMonth),
+                    PageNumber = 1,
+                    PageSize = 1000
+                };
+                var result = await Handler.GetTransactionsByPeriod(request);
+                if (result.IsSuccess)
+                    Transactions = result.Data ?? [];
+            }
+            catch (Exception ex)
+            {
+                Snackbar.Add(ex.Message, Severity.Error);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+
+            IsBusy = false;
+        }
+
+        #endregion
     }
 }

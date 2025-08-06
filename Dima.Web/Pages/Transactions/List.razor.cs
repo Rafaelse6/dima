8513dffda90808c9046f6c9787556e5e2f5da400
Rafael Santos <rafa.05125@gmail.com>
2025-08-06 09:18:22 -0300

    List Transactions

---
+++ Dima.Web/Pages/Transactions/List.razor.cs
@@ -0,0 +1,40 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Dima.Web.Pages.Transactions
+{
+    public partial class ListTransactionsPage : ComponentBase
+    {
+        #region Properties
+
+        public bool IsBusy { get; set; } = false;
+        public List<Transaction> Transactions { get; set; } = [];
+        public string SearchTerm { get; set; } = string.Empty;
+        public int CurrentYear { get; set; } = DateTime.Now.Year;
+        public int CurrentMonth { get; set; } = DateTime.Now.Month;
+
+        public int[] Years { get; set; } =
+        {
+            DateTime.Now.Year,
+            DateTime.Now.Year - 1,
+            DateTime.Now.Year - 2,
+            DateTime.Now.Year - 3,
+        };
+
+        #endregion
+
+        #region Services
+
+        [Inject]
+        public ISnackbar Snackbar { get; set; } = null!;
+
+        [Inject]
+        public IDialogService DialogService { get; set; } = null!;
+
+        [Inject]
+        public ITransactionHandler Handler { get; set; } = null!;
+        #endregion 
+    }
+}

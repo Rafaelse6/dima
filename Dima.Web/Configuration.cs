5a3ea6ef7516414425093b70da1b53d29114612d
Rafael Santos <rafa.05125@gmail.com>
2026-01-17 20:26:24 -0300

    .NET 9.0 migration

--- Dima.Web/Configuration.cs
+++ Dima.Web/Configuration.cs
@@ -14,17 +14,17 @@ namespace Dima.Web
         {
             Typography = new Typography
             {
-                Default = new Default()
+                Default = new DefaultTypography()
                 {
                     FontFamily = ["Raleway", "sans-serif"]
                 }
             },
-            Palette = new PaletteLight
+            PaletteLight = new PaletteLight
             {
                 Primary =  new MudColor("#1EFA2D"),
                 PrimaryContrastText = new MudColor("#000000"),
                 Secondary = Colors.LightGreen.Darken3,
-                Background = Colors.Grey.Lighten4,
+                Background = Colors.Gray.Lighten4,
                 AppbarBackground = new MudColor("#1EFA2D"),
                 AppbarText = Colors.Shades.Black,
                 TextPrimary = Colors.Shades.Black,
